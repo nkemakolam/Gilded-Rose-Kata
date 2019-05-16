4c8adbd726e9cea60ce32152841eed20e09c82f9
StreetShack <nkemakolamnnadi3@gmail.com>
2019-05-16 22:16:36 +0200

    Initial Refactoring

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -6,41 +6,39 @@ namespace GildedRose.Console
     {
         // refactoring variable parameter
 
-        
+        public IList<Item> Items;
 
-        IList<Item> Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
             var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                }
 
-                          };
+            };
 
             app.UpdateQuality();
 
             System.Console.ReadKey();
 
         }
-        
-        public void UpdateQuality()
-            //what needs to happen every day 
 
+        //what needs to happen every day 
+        public void UpdateQualityOriginal()
         {
             for (var i = 0; i < Items.Count; i++)
             {
@@ -117,8 +115,81 @@ namespace GildedRose.Console
             }
         }
 
-    }
+        public void UpdateQuality()
+        {
+            foreach(var item in Items)
+            {
+                var isAgedBrie = item.Name.Contains("Aged Brie");
+                var isBackstagePass = item.Name.Contains("Backstage pass");
+                var isSulfuras = item.Name.Contains("Sulfuras");
+                var isConjured = item.Name.Contains("Conjuras");
+                var isNormal = !isAgedBrie
+                    && !isBackstagePass
+                    && !isSulfuras
+                    && !isConjured;
+                
+                //The legend itself!
+                if (isSulfuras)
+                    continue;
 
-   
+                else
+                {
+                    item.SellIn--;
+
+                    if (isAgedBrie)
+                    {
+                        //Quality never exceeds 50
+                        if (item.Quality == 50)
+                            continue;
+
+                        //Quality increases as time passes
+                        else item.Quality++;
+                    }
+                    else if(isBackstagePass)
+                    {
+                        //Quality drops to zero after the concert
+                        if (item.SellIn < 0)
+                            item.Quality = 0;
+
+                        else
+                        {
+                            //Quality never exceeds 50
+                            if (item.Quality == 50)
+                                continue;
+
+                            //5 days or less to concert 
+                            else if (item.SellIn <= 5)
+                                item.Quality += 3;
+
+                            //10 days or less to concert
+                            else if (item.SellIn <= 10)
+                                item.Quality += 2;
+
+                            //Quality is never above 50
+                            if (item.Quality > 50)
+                                item.Quality = 50;
+                        }
+                    }
+                    else //normal or conjured
+                    {
+                        //degrade twice as fast after deadline, by a unit before dead line
+                        var degradeDegree = item.SellIn < 0 ? 2 : 1;
+
+                        //degrade normally
+                        item.Quality -= degradeDegree;
+
+                        //conjured degrades twice as fast as normal items
+                        if (isConjured)
+                            item.Quality -= degradeDegree;
+
+                        //Quality never drops below 0
+                        if (item.Quality < 0)
+                            item.Quality = 0;
+                    }
+                }
+            }
+        }
+
+    }
 
 }
--- src/GildedRose.Tests/obj/Debug/GitVersionTaskAssemblyInfo.g.cs
+++ src/GildedRose.Tests/obj/Debug/GitVersionTaskAssemblyInfo.g.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0+2.Branch.master.Sha.2ea2a8111482e097d4509a85c0431ecbf213f704")]
+[assembly: AssemblyInformationalVersion("0.1.0+3.Branch.master.Sha.3df9fa6db5cbbe54a4ab70b724b9ab76ef948db5")]
 
 namespace GildedRose.Tests
 {
@@ -28,22 +28,22 @@ namespace GildedRose.Tests
         public static string PreReleaseTagWithDash = "";
         public static string PreReleaseLabel = "";
         public static string PreReleaseNumber = "";
-        public static string BuildMetaData = "2";
-        public static string BuildMetaDataPadded = "0002";
-        public static string FullBuildMetaData = "2.Branch.master.Sha.2ea2a8111482e097d4509a85c0431ecbf213f704";
+        public static string BuildMetaData = "3";
+        public static string BuildMetaDataPadded = "0003";
+        public static string FullBuildMetaData = "3.Branch.master.Sha.3df9fa6db5cbbe54a4ab70b724b9ab76ef948db5";
         public static string MajorMinorPatch = "0.1.0";
         public static string SemVer = "0.1.0";
         public static string LegacySemVer = "0.1.0";
         public static string LegacySemVerPadded = "0.1.0";
         public static string AssemblySemVer = "0.1.0.0";
-        public static string FullSemVer = "0.1.0+2";
-        public static string InformationalVersion = "0.1.0+2.Branch.master.Sha.2ea2a8111482e097d4509a85c0431ecbf213f704";
+        public static string FullSemVer = "0.1.0+3";
+        public static string InformationalVersion = "0.1.0+3.Branch.master.Sha.3df9fa6db5cbbe54a4ab70b724b9ab76ef948db5";
         public static string BranchName = "master";
-        public static string Sha = "2ea2a8111482e097d4509a85c0431ecbf213f704";
+        public static string Sha = "3df9fa6db5cbbe54a4ab70b724b9ab76ef948db5";
         public static string NuGetVersionV2 = "0.1.0";
         public static string NuGetVersion = "0.1.0";
-        public static string CommitsSinceVersionSource = "2";
-        public static string CommitsSinceVersionSourcePadded = "0002";
+        public static string CommitsSinceVersionSource = "3";
+        public static string CommitsSinceVersionSourcePadded = "0003";
         public static string CommitDate = "2019-05-15";
     }
 
